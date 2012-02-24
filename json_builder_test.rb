6ca21e77b80d60619c863fcf447626db676dde22
Diego Plentz <diego@plentz.org>
2012-02-24 01:54:49 -0200

    making json_builder test a bit better

--- json_builder_test.rb
+++ json_builder_test.rb
@@ -7,10 +7,17 @@ require 'minitest/autorun'
 require 'json_builder'
 
 class JsonBuilderTest < MiniTest::Unit::TestCase
-  def test_json_generation
+  def test_json_generation_with_unicode_in_key
     json = JSONBuilder::Compiler.generate do
       é "json"
     end
     assert_equal '{"é": "json"}', json
+  end
+
+  def test_json_generation_with_unicode_in_value
+    json = JSONBuilder::Compiler.generate do
+      json "é"
+    end
+    assert_equal '{"json": "é"}', json
   end
 end
