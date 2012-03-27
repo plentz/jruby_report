cbecc99522e636f283184dbb4cd5d286ba521e8c
Diego Plentz <diego@plentz.org>
2012-03-27 03:47:19 -0300

    adding test for flori/json#101

--- flori_json_test.rb
+++ flori_json_test.rb
@@ -2,12 +2,22 @@
 
 require 'rubygems'
 require 'minitest/autorun'
+
 require 'json/ext' #this fails
 # require 'json/pure' #this works
 
 class FloriJsonTest < MiniTest::Unit::TestCase
+
+  #flori/json#97
   def test_json_encoder
     json = JSON.generate({'message' => "á"})
     assert_equal("{\"message\":\"á\"}", json)
   end
+
+  #flori/json#101
+  def test_encoding_after_encoding
+    hash = {"å"=>1}
+    assert_equal(Encoding::UTF_8, hash.to_json.encoding)
+  end
+
 end
